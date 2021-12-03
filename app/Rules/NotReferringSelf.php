48f78b1a22a509a34fcd16f6563899b1247dceb9
Volodymyr Zhonchuk <vzhonchuk@gmail.com>
2021-12-03 19:01:13 +0200

    :sparkles: Chore: Rules added
    Rules added

---
+++ app/Rules/NotReferringSelf.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use App\Models\User;
+
+class NotReferringSelf implements Rule
+{
+    protected $user;
+    
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        if ($value === $this->user->email) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'It is impossible to send a referral to yourself.';
+    }
+}
